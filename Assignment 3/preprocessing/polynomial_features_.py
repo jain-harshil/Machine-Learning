bc9e14ca3a4bc15501493b382efc941438b55023
Harshil Jain <jain.harshil@iitgn.ac.in>
2020-03-15 19:44:49 +0530

    Add files via upload

---
+++ Assignment 3/preprocessing/polynomial_features_.py
@@ -0,0 +1,40 @@
+''' In this file, you will utilize two parameters degree and include_bias.
+    Reference https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html
+'''
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class PolynomialFeatures():
+    
+    def __init__(self, degree=2,include_bias=True):
+        """
+        Inputs:
+        param degree : (int) max degree of polynomial features
+        param include_bias : (boolean) specifies wheter to include bias term in returned feature array.
+        """
+        
+        self.degree = degree
+        self.include_bias = include_bias
+    
+    def transform(self,X):
+        """
+        Transform data to polynomial features
+        Generate a new feature matrix consisting of all polynomial combinations of the features with degree less than or equal to the specified degree. 
+        For example, if an input sample is  np.array([a, b]), the degree-2 polynomial features with "include_bias=True" are [1, a, b, a^2, b^2].
+        
+        Inputs:
+        param X : (np.array) Dataset to be transformed
+        
+        Outputs:
+        returns (np.array) Tranformed dataset.
+        """
+        X = list(X)
+        a = []
+        if self.include_bias == True:
+            a.append(1)
+        for j in range (self.degree):
+            for i in range (len(X)):
+                a.append(X[i]**(j+1))
+        a = np.array(a)
+        return a
